f46ce6f2674748441255fe8666eea06df6446964
Rahul Ashokkumar Macwan <rh340172@dal.ca>
2022-11-25 23:10:40 -0400

    Creating Home page

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home  () {
+
+    const navigate = useNavigate()
+  return (
+    <div className="App">
+      <h1>Welcome to Halifax Foodie</h1>
+      
+        <button onClick={() => navigate(`/login`)}>Login</button>
+      
+        <button onClick={() => navigate(`/register`)}>Register</button>
+      
+        
+    </div>
+  );
+}
+
+export default Home;
